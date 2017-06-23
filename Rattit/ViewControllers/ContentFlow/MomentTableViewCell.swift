be7c11c93ea00a0bb26e0b0481421bc9ec860cd0
kaileding <dingkaile@gmail.com>
2017-06-23 10:58:52 -0700

    resolved all layout constraint conflicts

--- Rattit/ViewControllers/ContentFlow/MomentTableViewCell.swift
+++ Rattit/ViewControllers/ContentFlow/MomentTableViewCell.swift
@@ -16,8 +16,9 @@ class MomentTableViewCell: UITableViewCell {
     
     var momentPhotoScrollView: PhotoScrollView! = PhotoScrollView.instantiateFromXib()
     
-    var wordsLabelBottomToSuperViewConstraint: NSLayoutConstraint? = nil
-    var photoScrollViewBottomToSuperViewConstraint: NSLayoutConstraint? = nil
+    var wordsLabelTopToHeaderViewConstraint: NSLayoutConstraint? = nil
+    var photoScrollViewBottomToWordsLabelConstraint: NSLayoutConstraint? = nil
+    var photoScrollViewTopToHeaderViewConstraint: NSLayoutConstraint? = nil
     
     
     override func awakeFromNib() {
@@ -38,16 +39,17 @@ class MomentTableViewCell: UITableViewCell {
         self.momentWordsLabel.numberOfLines = 4
         self.momentWordsLabel.font = UIFont(name: "Helvetica", size: 14.0)
         self.momentWordsLabel.textColor = UIColor.darkText
+        self.momentWordsLabel.removeFromSuperview()
         self.contentView.addSubview(self.momentWordsLabel)
-        self.momentWordsLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 44.0).isActive = true
+        self.momentWordsLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 0.0).isActive = true
         self.momentWordsLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-        self.momentWordsLabel.topAnchor.constraint(greaterThanOrEqualTo: self.momentHeaderView.bottomAnchor).isActive = true
-        self.wordsLabelBottomToSuperViewConstraint = NSLayoutConstraint(item: self.momentWordsLabel, attribute: .bottom, relatedBy: .equal, toItem: self.contentView, attribute: .bottomMargin, multiplier: 1.0, constant: 0.0)
-        self.wordsLabelBottomToSuperViewConstraint?.isActive = true
+        self.wordsLabelTopToHeaderViewConstraint = NSLayoutConstraint(item: self.momentWordsLabel, attribute: .top, relatedBy: .equal, toItem: self.momentHeaderView, attribute: .bottomMargin, multiplier: 1.0, constant: 0.0)
+        self.momentWordsLabel.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
         
         // setup momentPhotoScrollView
         self.momentPhotoScrollView.translatesAutoresizingMaskIntoConstraints = false
-        
+        self.photoScrollViewTopToHeaderViewConstraint = NSLayoutConstraint(item: self.momentPhotoScrollView, attribute: .top, relatedBy: .equal, toItem: self.momentHeaderView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        self.photoScrollViewBottomToWordsLabelConstraint = NSLayoutConstraint(item: self.momentPhotoScrollView, attribute: .bottom, relatedBy: .equal, toItem: self.momentWordsLabel, attribute: .top, multiplier: 1.0, constant: 0.0)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -65,21 +67,30 @@ class MomentTableViewCell: UITableViewCell {
         if let photos = moment.photos, photos.count > 0 {
             self.momentPhotoScrollView.removeFromSuperview()
             self.contentView.addSubview(self.momentPhotoScrollView)
+            
+            self.wordsLabelTopToHeaderViewConstraint?.isActive = false
+            
             let margins = self.contentView.layoutMarginsGuide
             self.momentPhotoScrollView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: -8.0).isActive = true
             self.momentPhotoScrollView.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 8.0).isActive = true
-            self.momentPhotoScrollView.topAnchor.constraint(equalTo: self.momentWordsLabel.bottomAnchor, constant: 2.0).isActive = true
-            self.momentPhotoScrollView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: 0.0).isActive = true
-            self.wordsLabelBottomToSuperViewConstraint?.isActive = false
-            self.photoScrollViewBottomToSuperViewConstraint = NSLayoutConstraint(item: self.momentPhotoScrollView, attribute: .height, relatedBy: .equal, toItem: self.momentPhotoScrollView, attribute: .width, multiplier: 1.0, constant: 0.0)
-            self.photoScrollViewBottomToSuperViewConstraint?.isActive = true
+            let aspectRatioConstraint = NSLayoutConstraint(item: self.momentPhotoScrollView, attribute: .width, relatedBy: .equal, toItem: self.momentPhotoScrollView, attribute: .height, multiplier: 1.0, constant: 0.0)
+            aspectRatioConstraint.isActive = true
+            
+            self.photoScrollViewTopToHeaderViewConstraint?.isActive = true
+            self.photoScrollViewBottomToWordsLabelConstraint?.isActive = true
+            
             self.momentPhotoScrollView.sizeToFit()
+            self.momentWordsLabel.sizeToFit()
             
             self.momentPhotoScrollView?.initializeData(photos: photos, sideLength: sideLength)
         } else {
             self.momentPhotoScrollView.removeFromSuperview()
-            self.photoScrollViewBottomToSuperViewConstraint?.isActive = false
-            self.wordsLabelBottomToSuperViewConstraint?.isActive = true
+            
+            self.photoScrollViewTopToHeaderViewConstraint?.isActive = false
+            self.photoScrollViewBottomToWordsLabelConstraint?.isActive = false
+            
+            self.wordsLabelTopToHeaderViewConstraint?.isActive = true
+            
         }
         
     }
